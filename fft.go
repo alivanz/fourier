39de17631b289a4a3b606ac1b114258b349cbcb9
alivan <alivan1627@gmail.com>
2020-12-25 22:49:39 +0700

    simplify fft

--- fft.go
+++ fft.go
@@ -7,7 +7,10 @@ import (
 func NewFFT(n int) *CoefPairComplex {
 	inv := make([][]complex128, n)
 	for i := 0; i < n; i++ {
-		inv[i] = FFTCoefs(i, n)
+		inv[i] = make([]complex128, n)
+		for j := 0; j < n; j++ {
+			inv[i][j] = Expi(2 * math.Pi * float64(i) * float64(j) / float64(n))
+		}
 	}
 	l := float64(n)
 	coef := make([][]complex128, n)
@@ -30,18 +33,6 @@ func Expi(x float64) complex128 {
 	)
 }
 
-func FFTCoef(i, x, n int) complex128 {
-	return Expi(2 * math.Pi * float64(i) * float64(x) / float64(n))
-}
-
-func FFTCoefs(x, n int) []complex128 {
-	out := make([]complex128, n)
-	for i := 0; i < n; i++ {
-		out[i] = FFTCoef(i, x, n)
-	}
-	return out
-}
-
 func FFT(f []complex128) []complex128 {
 	fft := NewFFT(len(f))
 	return fft.Transform.Do(f)
@@ -50,13 +41,4 @@ func FFT(f []complex128) []complex128 {
 func IFFT(fs []complex128) []complex128 {
 	fft := NewFFT(len(fs))
 	return fft.Inverse.Do(fs)
-}
-
-func IFFTOne(fs []complex128, x int) complex128 {
-	n := len(fs)
-	var total complex128
-	for i, c := range FFTCoefs(x, n) {
-		total += fs[i] * c
-	}
-	return total
 }
